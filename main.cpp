108012a00774aea9a2fb004208deb3631f44ef05
GavinBarill <gavinpcb@gmail.com>
2018-11-19 17:44:18 -0500

    fixed main function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
-#include "UT_SolidAngle.h"
+// Copyright (C) 2018 Alec Jacobson <alecjacobson@gmail.com>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "WindingNumber/UT_SolidAngle.h"
 
 #include <igl/read_triangle_mesh.h>
 #include <igl/parallel_for.h>
